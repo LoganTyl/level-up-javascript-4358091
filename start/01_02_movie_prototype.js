ba95258ccf6a0c00b7584fa2cb6b90aee6f1e658
Logan Tyler <47304609+LoganTyl@users.noreply.github.com>
2023-07-13 17:48:17 +0000

    My solution for problem 2

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,12 @@
 // Write your code here
+function Movie(title, director, genre, releaseYear, rating) {
+  this.title = title;
+  this.director = director;
+  this.genre = genre;
+  this.releaseYear = releaseYear;
+  this.rating = rating;
+}
+Movie.prototype.getOverview = () => {
+  return `${this.title}, a ${this.genre} film directed by ${this.director} 
+  was released in ${this.releaseYear}. It received a rating of ${this.rating}.`
+}
