cebdd14ebab0017dc803931a0a4799eea999ada8
Logan Tyler <47304609+LoganTyl@users.noreply.github.com>
2023-07-13 17:15:00 +0000

    My solution for problem 1

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,33 @@
 // Write your code here
+class Book {
+  constructor(title, author, isbn, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.numCopies = numCopies;
+  }
+
+  get getAvailability() {
+    if(this.numCopies === 0) {
+      return "Out of Stock"
+    }
+    else if (this.numCopies < 10) {
+      return "Low Stock"
+    }
+    else {
+      return "In Stock"
+    }
+  }
+
+  set sell(numSold=1) {
+    if(this.numCopies - numSold < 0) {
+      console.log("Not enough copies to sell")
+      return;
+    }
+    this.numCopies -= numSold;
+  }
+
+  set restock(numCopies=5) {
+    this.numCopies += numCopies;
+  }
+}
