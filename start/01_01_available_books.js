b1fb622c9ef60ee3ebcdd79de9a65ef212aea6d5
Logan Tyler <47304609+LoganTyl@users.noreply.github.com>
2023-07-13 17:41:53 +0000

    Wrote instructor's solution for problem 1

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,5 +1,5 @@
 // Write your code here
-class Book {
+class MyBook {
   constructor(title, author, isbn, numCopies) {
     this.title = title;
     this.author = author;
@@ -31,3 +31,66 @@ class Book {
     this.numCopies += numCopies;
   }
 }
+
+// Instructor Solution
+// function Book(title, author, ISBN, numCopies){
+//   this.title = title;
+//   this.author = author;
+//   this.ISBN = ISBN;
+//   this.numCopies = numCopies;
+// }
+
+// Book.prototype.getAvailability = function() {
+//   if(this.numCopies === 0) {
+//     return "Out of stock";
+//   } else if (this.numCopies < 10) {
+//     return "Low stock";
+//   }
+//   return "In Stock";
+// }
+
+// Book.prototype.sell =  function(numCopiesSold = 1) {
+//   this.numCopies -= numCopiesSold;
+// }
+
+// Book.prototype.restock = function(numCopiesStocked = 5) {
+//   this.numCopies += numCopiesStocked;
+// }
+
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  get availability() {
+    return this.getAvailability();
+  }
+
+  getAvailability() {
+    if(this.numCopies === 0) {
+      return "Out of Stock";
+    } else if (this.numCopies < 10) {
+      return "Low stock"
+    }
+
+    return "In stock"
+  }
+
+  sell(numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold;
+  }
+
+  restock(numCopiesStocked = 5) {
+    this.numCopies += numCopiesStocked;
+  }
+}
+
+const SampleBook = new Book("Hunger Games", "Suzanne Collins", 123919, 5);
+console.log(SampleBook.getAvailability());
+SampleBook.restock(12);
+console.log(SampleBook.getAvailability());
+SampleBook.sell(17)
+console.log(SampleBook.getAvailability());
