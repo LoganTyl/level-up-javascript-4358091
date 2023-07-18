901b6bf9c66f65787d41df8bdbc568c36b62112a
Logan Tyler <47304609+LoganTyl@users.noreply.github.com>
2023-07-18 19:34:37 +0000

    My solution for problem 7

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,22 @@
 // Write your code here
+getStudents = (classroom) => {
+  let hasTeachingAssistant = classroom.hasTeachingAssistant;
+  let classList = classroom.classList;
+
+  let [teacher, ...classListWithoutTeacher] = classList
+
+  if(!hasTeachingAssistant) {
+    return classListWithoutTeacher;
+  }
+  let [ta, ...classListWithoutTA] = classListWithoutTeacher
+  return classListWithoutTA;
+}
+
+const classA = {
+  hasTeachingAssistant: false,
+  classList: [
+    "Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"
+  ]
+}
+
+console.log(getStudents(classA));
