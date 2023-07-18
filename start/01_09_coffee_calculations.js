cde4922c4ee6db15175f6b96a546431a47c77d86
Logan Tyler <47304609+LoganTyl@users.noreply.github.com>
2023-07-18 20:37:13 +0000

    My solution for problem 9

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,14 @@
 // Write your code here
+const getTotalCoffeePrice = (intArray) => {
+  const coffeePrice = 1.25;
+
+  let total = intArray.reduce(getTotal, 0);
+
+  function getTotal(total, num) {
+    return total + (num*coffeePrice);
+  }
+
+  return `The total bill is ${total}`;
+}
+
+console.log(getTotalCoffeePrice([2,3,1,5]));
