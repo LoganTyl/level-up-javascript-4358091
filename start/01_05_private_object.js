b263fb5b451234eb79ae6b83393c4deaf96d3038
Logan Tyler <47304609+LoganTyl@users.noreply.github.com>
2023-07-18 17:59:04 +0000

    Adding private variables outside of constructor

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,8 +1,16 @@
 // Write your code here
 class User {
+  #username;
+  #password;
+  
   constructor(username, password, age) {
     this.#username = username;
     this.#password = password;
     this.age = age;
   }
 }
+
+const testUser = new User("John", "Smith", 30);
+console.log(testUser.username);
+console.log(testUser.password);
+console.log(testUser.age);
